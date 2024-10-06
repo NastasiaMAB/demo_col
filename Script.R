b513aa1331ca3cb58029a2361ebf5ddd5e0a7858
Nastasia Baudin <nastasia.baudin@tutanota.com>
2024-10-05 23:49:37 -0600

    Added the R script into git index.

---
+++ Script.R
@@ -0,0 +1,4 @@
+# Load libraries
+# If microeco package is needed to be loaded run line 4. Otherwise do not load it as it is a BIG package.
+source("/Users/u1566746/Documents/Work/Rproject/libsource/mylibrary.R")
+# source("/Users/u1566746/Documents/Work/Rproject/libsource/mylibrary_microeco.R")
