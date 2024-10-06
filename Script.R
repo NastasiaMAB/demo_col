1ebf411058248abb398421e9f9c91226f835d425
Nastasia Baudin <nastasia.baudin@tutanota.com>
2024-10-06 01:38:23 -0600

    Updated script.

--- Script.R
+++ Script.R
@@ -1,4 +1,34 @@
-# Load libraries
-# If microeco package is needed to be loaded run line 4. Otherwise do not load it as it is a BIG package.
+### Set up R environment
+
+## Set up directory
+# !! Only if using R base, run line 6, 9, 12 and 15. Otherwise skip. !!
+# Reset R's brain
+# rm(list=ls())
+
+# getwd tells where R is currently looking
+# getwd()
+
+# setwd tell R where to look. Change 'the_directory' with actual path.
+# setwd("/Users/u1566746/Documents/Work/RProject/the_directory")
+
+# use getwd to confirm that R is now looking here
+# getwd()
+
+## Load libraries
 source("/Users/u1566746/Documents/Work/Rproject/libsource/mylibrary.R")
-# source("/Users/u1566746/Documents/Work/Rproject/libsource/mylibrary_microeco.R")
+
+# Show packages in mylib, the list of packages set to be loaded
+sort(mylib)
+
+# If microeco package is needed run line 20 to load it. Otherwise do not load it as it is a BIG package.
+# library(microeco)
+
+# List the libraries that are loaded
+sort((.packages()))
+
+## Import data
+# read the CSV file containing the data
+read.csv("data.csv")
+
+# Request to import data and store the data as an object, with a name, in R's brain
+dt<-read.csv("data.csv")
